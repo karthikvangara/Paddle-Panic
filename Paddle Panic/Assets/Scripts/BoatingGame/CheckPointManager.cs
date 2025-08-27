3914bf9ae7c6ff73cc3806ccc407916aaa745d55
karthikvangara <vangarakarthik29@gmail.com>
2025-08-27 20:21:33 +0530

    Changes

--- Paddle Panic/Assets/Scripts/BoatingGame/CheckPointManager.cs
+++ Paddle Panic/Assets/Scripts/BoatingGame/CheckPointManager.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class CheckPointManager : MonoBehaviour
 {
+    public HealthManager healthManager;
+    public RiverMapController riverMapController;
     public Vector3 recentCheckpointPosition;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("CheckPoint"))
         {
+            //Debug.Log("Karthik Checkpoint exited ");
+            if (!healthManager.isRespawning)
+            {
+                riverMapController.currentPlayersMapIndex += 1;
+                riverMapController.RespawnRiverMaps();
+            }
             recentCheckpointPosition = new Vector3(other.gameObject.transform.position.x, transform.position.y, other.gameObject.transform.position.z);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("CheckPoint"))
+        {
+            healthManager.isRespawning = false;
         }
     }
 }
