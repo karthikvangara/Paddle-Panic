96a32fcf1a4f7b00ea1e29f5b4a870af0847a757
Karthik <karthikbiggames@gmail.com>
2025-09-12 15:07:31 +0530

    RiverMaps Spawn Controlled

--- PaddlePanic/Assets/ScriptableObjects/RiverMapsSO.cs
+++ PaddlePanic/Assets/ScriptableObjects/RiverMapsSO.cs
@@ -5,7 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="RiverMapsSO", menuName ="RiverMapsSO")]
 public class RiverMapsSO : ScriptableObject
 {
-    public List<GameObject> easyRiverMapPrefabs;
-    public List<GameObject> mediumRiverMapPrefabs;
-    public List<GameObject> hardRiverMapPrefabs;
+    public List<RiverMap> riverMapPrefabsFromEasyToHard=new List<RiverMap>();
+}
+
+[System.Serializable]
+public class RiverMap
+{
+    public GameObject Map;
+    public RiverMapDifficulty mapDifficulty;
+    public GameObject SceneInstance;
+}
+
+public enum RiverMapDifficulty
+{
+    Easy,
+    Medium,
+    Hard
 }
